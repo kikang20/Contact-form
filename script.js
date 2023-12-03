b3c58abe715e9edf4ac249e8238391754ab26b9c
ki kang <kikang20@yahoo.com>
2023-12-03 00:24:43 -0800

    voyage

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-const subjectInput = document.getElementById('subject');
 
-    subjectInput.addEventListener('click', () => {
-      alert('Please accept me for yoyage 47!');
-    });
+
+    const form = document.querySelector('form');
+
+    // Add event listener for form submission
+    form.addEventListener('submit', validateForm);
+    
+    // Function to validate the form
+    function validateForm(event) {
+      // Prevent the form from being submitted to the server
+      event.preventDefault();
+    
+      // Get the input values
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      const title = document.getElementById('title').value;
+      const subject = document.getElementById('subject').value;
+    
+      // Perform your validation logic here
+      if (name && email && title && subject) {
+        // All fields are filled, form is valid
+        console.log('Form is valid');
+        return true;
+      } else {
+        // One or more fields are empty, form is invalid
+        console.log('Please fill out everything correctly.');
+        return false;
+      }
+    }
