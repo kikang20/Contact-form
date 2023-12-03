8ed8edc1b3828b4d3cec57eae04bf2e4e19de2ad
ki kang <kikang20@yahoo.com>
2023-12-03 01:03:34 -0800

    voyage

--- script.js
+++ script.js
@@ -1,29 +1,22 @@
+const form = document.querySelector('form');
 
 
-    const form = document.querySelector('form');
+form.addEventListener('submit', validateForm);
 
-    // Add event listener for form submission
-    form.addEventListener('submit', validateForm);
+function validateForm(event) {
+  event.preventDefault();
+
+ 
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const title = document.getElementById('title').value;
+  const subject = document.getElementById('subject').value;
+
+
+  if (name && email && title && subject) {
+    console.log('Form is valid');
+  } else {
+    console.log('Please fill out everything correctly.');
     
-    // Function to validate the form
-    function validateForm(event) {
-      // Prevent the form from being submitted to the server
-      event.preventDefault();
-    
-      // Get the input values
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      const title = document.getElementById('title').value;
-      const subject = document.getElementById('subject').value;
-    
-      // Perform your validation logic here
-      if (name && email && title && subject) {
-        // All fields are filled, form is valid
-        console.log('Form is valid');
-        return true;
-      } else {
-        // One or more fields are empty, form is invalid
-        console.log('Please fill out everything correctly.');
-        return false;
-      }
-    }
+  }
+} 
